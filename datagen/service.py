c11618ba170898cb6b167d3057bbe080cbd9e340
Taehee Lee <prussian1933@naver.com>
2024-04-17 13:22:13 +0900

    테이블 베이스 스크립트

--- datagen/service.py
+++ datagen/service.py
@@ -13,10 +13,42 @@ def orders():
     cursor = db.cursor()
 
     st = Statements('mysql-statements.xml')
-    sql = st.get("select_all_from_accounts")
 
-    cursor.execute(sql)
-    df = pd.DataFrame(cursor.fetchall(), columns=[i[0] for i in cursor.description])
+    # [ [ 계정, [매장 -> 배달 정보 ] ] -> 주문, 장바구니 ] -> 주문 라인
+    # query accounts to order
+    cursor.execute(st.get("query_account_to_order"))
+    account_df = pd.DataFrame(cursor.fetchall(), columns=[i[0] for i in cursor.description])
 
-    print(df)
+    # query stores to order
+    cursor.execute(st.get("query_store_to_order"))
+    store_df = pd.DataFrame(cursor.fetchall(), columns=[i[0] for i in cursor.description])
 
+    # query delivery info of store to order
+    cursor.execute(st.get("query_delivery_info_of_store_to_order"))
+    delivery_info_df = pd.DataFrame(cursor.fetchall(), columns=[i[0] for i in cursor.description])
+
+    # query carts of cart to order line
+    cursor.execute(st.get("query_carts_of_cart_to_orderline"))
+    carts_df = pd.DataFrame(cursor.fetchall(), columns=[i[0] for i in cursor.description])
+
+    # create order with account, store, delivery info, order line
+    cursor.execute(st.get("insert_order", param={
+        "id": "",
+        "accountId": "",
+        "storeId": "",
+        "status": "",
+        ...
+    }))
+
+    # create order line with order, product
+    cursor.execute(st.get("insert_orderline", param={
+        "id": "",
+        "orderId": "",
+        "productId": "",
+        "option": "",
+        "amount": "",
+        "createdOn": "",
+        "lastUpdatedOn": ""
+    }))
+
+    cursor.execute(st.get("update_order_placement"))
